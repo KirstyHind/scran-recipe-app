bfd267d4bc49c4e79566964bce0d236063ce3b6d
Kirsty181 <116749589+Kirsty181@users.noreply.github.com>
2023-07-28 11:28:33 +0100

    Your commit message here

--- SavedRecipes.js
+++ SavedRecipes.js
@@ -6,94 +6,94 @@ import { getAuth } from 'firebase/auth';
 import Toolbar from './Toolbar';
 
 const SavedRecipes = () => {
-  const navigation = useNavigation();
-  const [savedRecipes, setSavedRecipes] = useState([]);
-  
-  const fetchSavedRecipes = () => {
-    const auth = getAuth();
-    const database = getDatabase();
-    const savedRecipesRef = ref(database, `users/${auth.currentUser.uid}/savedRecipes`);
+    const navigation = useNavigation();
+    const [savedRecipes, setSavedRecipes] = useState([]);
 
-    const onValueChange = snapshot => {
-      if (snapshot.exists()) {
-        const data = snapshot.val();
-        const allSavedRecipes = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
+    const fetchSavedRecipes = () => {
+        const auth = getAuth();
+        const database = getDatabase();
+        const savedRecipesRef = ref(database, `users/${auth.currentUser.uid}/savedRecipes`);
 
-        setSavedRecipes(allSavedRecipes);
-      } else {
-        setSavedRecipes([]);
-      }
-    }
+        const onValueChange = snapshot => {
+            if (snapshot.exists()) {
+                const data = snapshot.val();
+                const allSavedRecipes = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
 
-    onValue(savedRecipesRef, onValueChange);
+                setSavedRecipes(allSavedRecipes);
+            } else {
+                setSavedRecipes([]);
+            }
+        }
 
-    // Return a cleanup function to remove the listener
-    return () => off(savedRecipesRef, 'value', onValueChange);
-  };
+        onValue(savedRecipesRef, onValueChange);
 
-  useEffect(() => {
-    const cleanup = fetchSavedRecipes();
+        // Return a cleanup function to remove the listener
+        return () => off(savedRecipesRef, 'value', onValueChange);
+    };
 
-    return cleanup;
-  }, []);
+    useEffect(() => {
+        const cleanup = fetchSavedRecipes();
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Saved Recipes</Text>
-      {savedRecipes.length > 0 ? (
-        savedRecipes.map((recipe) => (
-          <TouchableOpacity key={recipe.id} onPress={() => navigation.navigate('RecipeDetails', { recipeId: recipe.id })}>
-            <View style={styles.recipeContainer}>
-              <Image source={{ uri: recipe.image }} style={styles.recipeImage} />
-              <View style={styles.recipeDetails}>
-                <Text style={styles.recipeName}>{recipe.recipeName}</Text>
-                <Text style={styles.recipeDescription}>{recipe.description}</Text>
-              </View>
-            </View>
-          </TouchableOpacity>
-        ))
-      ) : (
-        <Text style={styles.noRecText}>No saved recipes.</Text>
-      )}
+        return cleanup;
+    }, []);
 
-      {/* Toolbar */}
-      <Toolbar />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Saved Recipes</Text>
+            {savedRecipes.length > 0 ? (
+                savedRecipes.map((recipe) => (
+                    <TouchableOpacity key={recipe.id} onPress={() => navigation.navigate('RecipeDetails', { recipeId: recipe.id })}>
+                        <View style={styles.recipeContainer}>
+                            <Image source={{ uri: recipe.image }} style={styles.recipeImage} />
+                            <View style={styles.recipeDetails}>
+                                <Text style={styles.recipeName}>{recipe.recipeName}</Text>
+                                <Text style={styles.recipeDescription}>{recipe.description}</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                ))
+            ) : (
+                <Text style={styles.noRecText}>No saved recipes.</Text>
+            )}
+
+            {/* Toolbar */}
+            <Toolbar />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    padding: 20,
-  },
-  noRecText: {
-    paddingLeft: 20,
-  },
-  recipeContainer: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  recipeImage: {
-    width: 100,
-    height: 100,
-    marginRight: 10,
-  },
-  recipeDetails: {
-    flex: 1,
-    padding: 20,
-  },
-  recipeName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  recipeDescription: {
-    fontSize: 16,
-  },
+    container: {
+        flex: 1,
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 20,
+    },
+    noRecText: {
+        paddingLeft: 20,
+    },
+    recipeContainer: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    recipeImage: {
+        width: 100,
+        height: 100,
+        marginRight: 10,
+    },
+    recipeDetails: {
+        flex: 1,
+        padding: 20,
+    },
+    recipeName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    recipeDescription: {
+        fontSize: 16,
+    },
 });
 
 export default SavedRecipes;
