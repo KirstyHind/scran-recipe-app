fce06edc19bf0ad66895dbbfd04728f6595d97b1
Kirsty181 <116749589+Kirsty181@users.noreply.github.com>
2023-08-01 15:28:21 +0100

    Your commit message here

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,7 +1,8 @@
 // Import necessary packages and components
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { getDatabase, ref, onValue, off } from 'firebase/database';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
 import Toolbar from './Toolbar';
 
@@ -17,6 +18,44 @@ const HomeScreen = () => {
 
   // State to store the search query
   const [searchQuery, setSearchQuery] = useState('');
+
+  // State to store saved recipes
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  // Fetch saved recipes function definition
+  const fetchSavedRecipes = () => {
+    // Get current user and database reference
+    const auth = getAuth();
+    const database = getDatabase();
+    const savedRecipesRef = ref(database, `users/${auth.currentUser.uid}/savedRecipes`);
+
+    // Define the value change handler
+    const onValueChange = snapshot => {
+      if (snapshot.exists()) {
+        // Transform the snapshot to a list of recipes
+        const data = snapshot.val();
+        const allSavedRecipes = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
+
+        setSavedRecipes(allSavedRecipes);
+      } else {
+        // Set empty recipes list if there's no data
+        setSavedRecipes([]);
+      }
+    }
+
+    // Attach the value change listener
+    onValue(savedRecipesRef, onValueChange);
+
+    // Cleanup function for removing the listener
+    return () => off(savedRecipesRef, 'value', onValueChange);
+  };
+
+  // Fetch saved recipes on component mount
+  useEffect(() => {
+    const cleanup = fetchSavedRecipes();
+
+    return cleanup;
+  }, []);
 
   // Function to handle logout
   const handleLogout = () => {
@@ -108,6 +147,26 @@ const HomeScreen = () => {
         </TouchableOpacity>
       </View>
 
+      {/* Saved Recipes ScrollView */}
+      <Text style={styles.heading}>Your Saved Recipes:</Text>
+      <ScrollView horizontal>
+        {savedRecipes.length > 0 ? (
+          savedRecipes.map((recipe) => (
+            <TouchableOpacity key={recipe.id} onPress={() => navigation.navigate('RecipeDetails', { recipeId: recipe.id })}>
+              <View style={styles.recipeContainer}>
+                <Image source={{ uri: recipe.image }} style={styles.recipeImage} />
+                <View style={styles.recipeDetails}>
+                  <Text style={styles.recipeName}>{recipe.recipeName}</Text>
+                  <Text style={styles.recipeDescription}>{recipe.description}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))
+        ) : (
+          <Text style={styles.noRecText}>No saved recipes.</Text>
+        )}
+      </ScrollView>
+
       {/* Toolbar */}
       <Toolbar />
     </View>
@@ -167,6 +226,35 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
+  },
+
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  noRecText: {
+    paddingLeft: 20,
+  },
+  recipeContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  recipeImage: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+  },
+  recipeDetails: {
+    flex: 1,
+    padding: 20,
+  },
+  recipeName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  recipeDescription: {
+    fontSize: 16,
   },
 });
 
