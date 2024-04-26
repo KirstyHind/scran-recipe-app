704fe3618f9086072cfec04e05566d897cfc6a2a
KirstyHind <kirsty.hind181@gmail.com>
2024-04-26 12:30:59 +0100

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,27 @@
 // Firebase core libraries
 import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 // Initialise Firebase
 const firebaseConfig = {
+
   apiKey: "AIzaSyCjPusB_sQbjyxI4DEolI4Pry6omeylmgs",
+
   authDomain: "scran-recipe-app-e4fb7.firebaseapp.com",
+
   databaseURL: "https://scran-recipe-app-e4fb7-default-rtdb.europe-west1.firebasedatabase.app",
+
   projectId: "scran-recipe-app-e4fb7",
+
   storageBucket: "scran-recipe-app-e4fb7.appspot.com",
+
   messagingSenderId: "976669814346",
+
   appId: "1:976669814346:web:a621cb4701c8d9c87a00b3"
+
 };
+
 
 let app;
 
@@ -23,6 +33,8 @@ if (!getApps().length) {
   // If already initialized, use the existing app
   app = getApp();
 }
+
+const analytics = getAnalytics(app);
 
 // Get a reference to the database service
 export const database = getDatabase(app);
